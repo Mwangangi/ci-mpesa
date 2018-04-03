f128292f98d272dd4d355d9718e336ee411ff159
Mwangangi <musyoki96@gmail.com>
2018-04-03 14:37:33 +0300

    Credentials config

--- application/config/mpesa.php
+++ application/config/mpesa.php
@@ -4,3 +4,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * MPESA API credentials
  */
+switch (variable) {
+	case 'value':
+		# code...
+		break;
+	
+	default:
+		# code...
+		break;
+}
