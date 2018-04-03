930aed9d6bc922177467f4ff8c092e7d124a1404
Mwangangi <musyoki96@gmail.com>
2018-04-03 14:45:22 +0300

    Fix git-ignore

--- application/config/mpesa.php
+++ application/config/mpesa.php
@@ -4,12 +4,4 @@ defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * MPESA API credentials
  */
-switch (variable) {
-	case 'value':
-		# code...
-		break;
-	
-	default:
-		# code...
-		break;
-}
+$config['credentials'] = "";
